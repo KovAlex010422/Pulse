f6abb25b5708511a83e44bb387eca3c4c7511f70
KovAl(desktop) <koval4ykoo1@gmail.com>
2023-03-03 22:57:03 +0200

    next is validation of form

--- src/js/modals.js
+++ src/js/modals.js
@@ -4,27 +4,26 @@ document.addEventListener('DOMContentLoaded', function(){
         modalConsult = document.querySelector('#consultation'),
         btnsOrder = document.querySelectorAll('[data-modal=order]'),
         modalOrder = document.querySelector('#order'),
-        modalClose = document.querySelectorAll('.modal__close');
+        modalClose = document.querySelectorAll('.modal__close'),
+        itemDescr = document.querySelectorAll(".catalog-item__subheader"),
+        modalOrderDescr = document.querySelector("#order .modal__descr");
 
   function openModal(btnsSelektor, modalWindow) {
     btnsSelektor.forEach((el, i) => el.addEventListener('click', () => {
-      overlay.style.display = 'block'
-      modalWindow.style.display = 'block'
-  
-    console.log(parentNode(btnsSelektor[i]));
+      overlay.style.display = 'block';
+      modalWindow.style.display = 'block';
+      modalOrderDescr.textContent = itemDescr[i].textContent;
     }))
   }
 
   openModal(btnsConsult, modalConsult);
   openModal(btnsOrder, modalOrder);
 
-
   modalClose.forEach(el => el.addEventListener('click', () => {
     overlay.style.display = 'none'
     modalConsult.style.display = 'none'
   })
   )
-
 })
 
 
