ef0059ceee0f48f092802d01373eb7262d975de3
KovAl(desktop) <koval4ykoo1@gmail.com>
2023-03-06 21:48:11 +0200

    do validation of form

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
   const formInputs = document.querySelectorAll('.input'),
-        formName = document.querySelectorAll('[form=Name]'),
-        btmSubmit = document.querySelector('[modal=consultation]'),
-        inputLabel = this.createElement('div');
-  function ad(){
-    formInputs.forEach(input => {
-      if(!input.value && !input.value.trim()){
-        console.log(input.value);
-        input.value = '1eafaf';
-        inputLabel.textContent=' Введи дані';
-        input.append(inputLabel)}
-      })
+        formName = document.querySelectorAll('[data-form=name]'),
+        formPhone = document.querySelectorAll('[data-form=phone]'),
+        formEmail = document.querySelectorAll('[data-form=email]'),
+        formSubmit = document.querySelectorAll('.feed-form'),
+        inputLabel = document.createElement('label');
+
+
+
+  function formSubmitFunc(forms){
+    console.log(forms);
+    forms.forEach(form => form.addEventListener('submit', e => {
+      e.preventDefault();
+
+      for(let elem of form.elements) {
+        if(elem.dataset.form) {
+          if(elem.value) {
+            console.log(elem.value)
+          }
+        }
+      }
+    }));
+
   }
-  ad()
+
+  formSubmitFunc(formSubmit)
 })
