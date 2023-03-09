c9c1ddc9f43fc8d87c358833a1a373284ec26feb
KovAl(desktop) <koval4ykoo1@gmail.com>
2023-03-09 21:37:48 +0200

    validation...

--- src/js/validation.js
+++ src/js/validation.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     formName = document.querySelector('[data-form=name]'),
     formPhone = document.querySelectorAll('[data-form=phone]'),
     formEmail = document.querySelectorAll('[data-form=email]'),
-    inputLabel = document.createElement('label'),
+    label = document.createElement('label'),
     bf = document.querySelector('.b-form'),
     bn = document.querySelector('.b-name'),
     bp = document.querySelector('.b-phone'),
@@ -16,9 +16,38 @@ document.addEventListener('DOMContentLoaded', function () {
     )
 
   function chekData(name, phone, email){
-    if(name.value.length < 2){
-      console.log('bad');
-    }
+    name.addEventListener('change', e => {
+      let nameVal = e.target.value;
+
+      if (nameVal.length < 2){
+        label.textContent = 'type name whith more 2 char';
+        label.classList.add('labelErr')
+        name.after(label)
+        name.classList.add('inputErr')
+      } else {
+        label.remove();
+        name.classList.remove('inputErr')
+        name.classList.add('inputCor')
+        return true
+      }
+    })
+    phone.addEventListener('change', e => {
+      let regEx = /^\d{5}$/;
+      let phoneVal = e.target.value;
+      console.log(phoneVal);
+    
+      if(!regEx.test(phoneVal)) {
+        label.textContent = 'введи тел номер (5 цифр)';
+        label.classList.add('labelErr')
+        phone.after(label)
+        phone.classList.add('inputErr')
+      } else {
+        label.remove();
+        phone.classList.remove('inputErr')
+        phone.classList.add('inputCor')
+        return true
+      }
+    })
   }
 
   chekData(bn, bp, be)
