b38d5ca16281cdb805204078e705d7ba1af6ba3d
KovAl(desktop) <koval4ykoo1@gmail.com>
2023-03-10 10:52:48 +0200

    next is validation all forms in html.

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const formInputs = document.querySelectorAll('.input'),
     forms = document.querySelectorAll('.feed-form'),
-    formName = document.querySelector('[data-form=name]'),
+    formName = document.querySelectorAll('[data-form=name]'),
     formPhone = document.querySelectorAll('[data-form=phone]'),
     formEmail = document.querySelectorAll('[data-form=email]'),
     label = document.createElement('label'),
@@ -10,120 +10,81 @@ document.addEventListener('DOMContentLoaded', function () {
     bp = document.querySelector('.b-phone'),
     be = document.querySelector('.b-email');
 
-    bf.addEventListener('submit', e => {
-      e.preventDefault()
-      }
-    )
 
-  function chekData(name, phone, email){
-    name.addEventListener('change', e => {
-      let nameVal = e.target.value;
-
-      if (nameVal.length < 2){
-        label.textContent = 'type name whith more 2 char';
-        label.classList.add('labelErr')
-        name.after(label)
-        name.classList.add('inputErr')
-      } else {
-        label.remove();
-        name.classList.remove('inputErr')
-        name.classList.add('inputCor')
-        return true
-      }
-    })
-    phone.addEventListener('change', e => {
-      let regEx = /^\d{5}$/;
-      let phoneVal = e.target.value;
-      console.log(phoneVal);
-    
-      if(!regEx.test(phoneVal)) {
-        label.textContent = 'введи тел номер (5 цифр)';
-        label.classList.add('labelErr')
-        phone.after(label)
-        phone.classList.add('inputErr')
-      } else {
-        label.remove();
-        phone.classList.remove('inputErr')
-        phone.classList.add('inputCor')
-        return true
-      }
-    })
+forms.forEach(form => form.addEventListener('submit', e => {
+  if (!checkName(bn) || !checkPhone(bp) || !checkEmail(be)) {
+    e.preventDefault()
+    console.log(e.target[1].value);
   }
-
-  chekData(bn, bp, be)
-
-
+}
+))
 
 
+formName.forEach(name => name.addEventListener('blur', e => checkName(name)));
+formPhone.forEach(phone => phone.addEventListener('blur', e => checkPhone(phone)));
+formEmail.forEach(email => email.addEventListener('blur', e => checkEmail(email)));
 
+function checkData(name, phone, email) {
+  name.addEventListener('blur', e => checkName(name))
+  phone.addEventListener('blur', e => checkPhone(phone))
+  email.addEventListener('blur', e => checkEmail(email))
+}
 
+function checkName(name) {
+  label.remove();
+  if (name.value.length < 2) {
+    label.textContent = 'type name whith more 2 char';
+    label.classList.add('labelErr')
+    name.after(label)
+    name.classList.remove('inputCor')
+    name.classList.add('inputErr')
+    return false
+  } else {
+    label.remove();
+    name.classList.remove('inputErr')
+    name.classList.add('inputCor')
+    return true
+  }
+}
 
+function checkPhone(phone) {
+  let regEx = /^\d{5}$/;
 
+  if (!regEx.test(phone.value)) {
+    label.textContent = 'введи тел номер (5 цифр)';
+    label.classList.add('labelErr')
+    phone.after(label)
+    phone.classList.remove('inputCor')
+    phone.classList.add('inputErr')
+  } else {
+    label.remove();
+    phone.classList.remove('inputErr')
+    phone.classList.add('inputCor')
+    return true
+  }
+  return false
 
+}
 
+function checkEmail(email) {
+  let regEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  label.remove();
 
+  if (!regEx.test(email.value)) {
+    label.textContent = 'type correct email';
+    label.classList.add('labelErr')
+    email.after(label)
+    email.classList.remove('inputCor')
+    email.classList.add('inputErr')
+  } else {
+    label.remove();
+    email.classList.remove('inputErr')
+    email.classList.add('inputCor')
+    return true
+  }
+  return false
+}
 
+checkData(bn, bp, be)
 
-
-  // function formCheking(form) {
-  //   form.addEventListener('submit', e => {
-
-  //     e.preventDefault();
-  //   });
-    
-  //   const arrInputs = inputsFilter(form);
-
-  //   for (let inp of arrInputs) {
-  //     let inpData = inp.dataset.form;
-  //     // console.log(inp.value);
-  //     let inpTrim = inp.value.trim();
-  //     switch (inpData) {
-  //       case 'name':
-  //         inputsCheking(inp, inpData, inpTrim);
-  //         break;
-  //       case 'phone':
-  //         inputsCheking(inp, inpData);
-  //         break;
-  //       case 'email':
-  //         inputsCheking(inp, inpData);
-  //         break;
-
-  //       default:
-  //         break;
-  //     }
-  //   }
-  // }
-
-  // function inputsCheking(input, inputData, inpTrim) {
-  //   input.addEventListener('blur', e => {
-  //     if (inputData == 'name' && inpTrim.length < 2) {
-  //       input.style.border = '1px solid red'
-  //       inputLabel.textContent = 'Введи дві або більше букв';
-  //       inputLabel.style.fontSize = '16px'
-  //       inputLabel.style.color = 'white'
-  //       input.after(inputLabel);
-  //     } else if (inputData == 'phone') {
-  //       input.style.border = '1px solid red'
-  //     }
-  //     else {
-  //       input.style.border = '1px solid green'
-  //       inputLabel.remove()
-  //     }
-  //   })
-
-  // }
-  // // input.value.length == 0 && input.value.trim().length == 0
-
-  // function inputsFilter(arr) {
-  //   const arrInputs = [];
-  //   for (let input of arr) {
-  //     let inputVal = input.value;
-  //     if (input.tagName == 'INPUT') {
-  //       arrInputs.push(input);
-  //     }
-  //   }
-  //   return arrInputs
-  // }
-
-  // formCheking(bf)
 })
